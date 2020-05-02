78d640715c732279e3d4157ee665643b75e397e1
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-02 15:58:27 +0530

    UC2 Multiple coin flips done and heads and tails count display

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,13 +2,19 @@
 
 echo "Welcome to coin Simulator"
 IS_HEADS=1
-
+read -p "Enter the number of times to flip the coin :" repeatation
+headsCount=0
+tailsCount=0
+for ((counter=1;counter<=repeatation;counter++))
+do
 randomCoinFlip=$((RANDOM%2))
-
 if [ $randomCoinFlip -eq $IS_HEADS ]
 then
-	echo "Heads is the winner"
+	((headsCount++))
 else
-	echo "Tails is the winner"
+	((tailsCount++))
 fi
+done
+echo "Number of heads : $headsCount"
+echo "Number of tails : $tailsCount"
 
